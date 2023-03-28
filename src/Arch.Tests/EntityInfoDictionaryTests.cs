0d04cc5ff843aac2d27e118adf602f3c297fbaef
Lars <behethangames@gmail.com>
2023-03-28 22:05:25 +0200

    Added documentations to the latest PR #75

--- src/Arch.Tests/EntityInfoDictionaryTests.cs
+++ src/Arch.Tests/EntityInfoDictionaryTests.cs
@@ -3,33 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Arch.Core;
 using Arch.Core.Utils;
 
 namespace Arch.Tests;
 
+/// <summary>
+///     Tests the <see cref="EntityInfoDictionary"/> for its functionality.
+/// </summary>
 [TestFixture]
 public class EntityInfoDictionaryTests
 {
+
+    /// <summary>
+    ///     Checks whether the <see cref="EntityInfoDictionary"/> adds <see cref="EntityInfo"/> correctly.
+    /// </summary>
     [Test]
     public void Add()
     {
         var size = 1000;
         var entityInfo = new EntityInfoDictionary(size);
 
-        
-        for (int i = 0; i < size; i++)
+        // Adds
+        for (var i = 0; i < size; i++)
         {
             // Set the version to the current index.
             var version = i;
-
-            entityInfo.Add(i, new Core.EntityInfo(default, default, version));
+            entityInfo.Add(i, new EntityInfo(default, default, version));
         }
 
-        for (int i = 0; i < size; i++)
+        // Check
+        for (var i = 0; i < size; i++)
         {
             // Iterate over the dictionary if the index doesn't match the version then something went wrong
             var version = entityInfo[i].Version;
-
             if (version != i)
             {
                 Assert.Fail("Dictionary does not returns the correct object");
@@ -37,25 +44,28 @@ public class EntityInfoDictionaryTests
         }
     }
 
+    /// <summary>
+    ///     Checks whether the <see cref="EntityInfoDictionary"/> trims correctly.
+    /// </summary>
     [Test]
     public void TrimExcess()
     {
         var size = 10000;
         var entityInfo = new EntityInfoDictionary(size);
 
-        for(int i = 0; i < size; i++)
+        // Add infos
+        for(var i = 0; i < size; i++)
         {
             var version = i;
-
-            entityInfo.Add(i, new Core.EntityInfo(default, default, version));
+            entityInfo.Add(i, new EntityInfo(default, default, version));
         }
 
+        // Trims
         entityInfo.TrimExcess();
 
-        for (int i = 0; i < size ; i++)
+        for (var i = 0; i < size ; i++)
         {
             var version = entityInfo[i].Version;
-
             if(version != i)
             {
                 Assert.Fail($"{nameof(EntityInfoDictionary.TrimExcess)} method deleted items");
--- src/Arch/Core/Utils/EntityInfoDictionary.cs
+++ src/Arch/Core/Utils/EntityInfoDictionary.cs
@@ -8,17 +8,31 @@ using Arch.Core.Extensions;
 
 namespace Arch.Core.Utils;
 
+/// <summary>
+///     The <see cref="EntityInfoDictionary"/> class
+///     represents an jagged array that stores <see cref="EntityInfo"/> for quickly acessing it.
+/// </summary>
 internal class EntityInfoDictionary
 {
     /// <summary>
-    /// How large a chunk should be. This value will be a power of 2.
+    ///     How large a chunk should be. This value will be a power of 2.
     /// </summary>
     private static readonly int _chunkSize;
     private static readonly int _chunkSizeMinusOne;
 
-    private EntityInfo[][] _entityInfos = new EntityInfo[0][];
+    /// <summary>
+    ///     The jagged array storing the <see cref="EntityInfo"/>.
+    /// </summary>
+    private EntityInfo[][] _entityInfos = Array.Empty<EntityInfo[]>();
+
+    /// <summary>
+    ///     The currently largest id inside this <see cref="EntityInfoDictionary"/>, for trimming purposes.
+    /// </summary>
     private int _largestId;
 
+    /// <summary>
+    ///     Initializes the static values of <see cref="EntityInfoDictionary"/>.
+    /// </summary>
     static EntityInfoDictionary()
     {
         var cpuL1CacheSize = 16_000; // In bytes
@@ -28,47 +42,51 @@ internal class EntityInfoDictionary
         _chunkSizeMinusOne = _chunkSize - 1;
     }
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="EntityInfoDictionary"/> class.
+    /// </summary>
     public EntityInfoDictionary() : this(256)
     {
-
     }
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="EntityInfoDictionary"/> class.
+    /// </summary>
+    /// <param name="capacity">The initial capacity.</param>
     public EntityInfoDictionary(int capacity)
     {
         EnsureCapacity(capacity);
     }
 
+    /// <summary>
+    ///     Adds a new <see cref="EntityInfo"/> to a given index.
+    /// </summary>
+    /// <param name="id">The index.</param>
+    /// <param name="entityInfo">The <see cref="EntityInfo"/>.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Add(int id, EntityInfo entityInfo)
     {
         this[id] = entityInfo;
     }
 
+    /// <summary>
+    ///     Removes an <see cref="EntityInfo"/> from a given index.
+    ///     Replaces its value with the default one.
+    /// </summary>
+    /// <param name="id">The index.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Remove(int id)
     {
         this[id] = default;
     }
 
-    public void EnsureCapacity(int capacity)
-    {
-        if (capacity < _largestId)
-        {
-            return;
-        }
-
-        var currentSize = _entityInfos.Length;
-        var desiredSize = (capacity / _chunkSize) + 1;
-
-        Array.Resize(ref _entityInfos, desiredSize);
-
-        // Create the new arrays. 
-        for (int i = currentSize; i < desiredSize; i++)
-        {
-            _entityInfos[i] = new EntityInfo[_chunkSize];
-        }
-
-        UpdateLargestId();
-    }
-
+    /// <summary>
+    ///     Trys to return an <see cref="EntityInfo"/> from its index, if its set.
+    /// </summary>
+    /// <param name="id">The index.</param>
+    /// <param name="entityInfo">The <see cref="EntityInfo"/>.</param>
+    /// <returns>True if it was set, false if not.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool TryGetValue(int id, out EntityInfo entityInfo)
     {
         // If the id is negative
@@ -78,7 +96,7 @@ internal class EntityInfoDictionary
             return false;
         }
 
-        GetIndexesFromId(id, out var outerIndex, out var innerIndex);
+        IdToSlot(id, out var outerIndex, out var innerIndex);
 
         // If the item is outside the array. Then it definetly doesn't exist
         if (outerIndex > _entityInfos.Length)
@@ -100,10 +118,55 @@ internal class EntityInfoDictionary
         return true;
     }
 
+    /// <summary>
+    ///     Converts the passed id to its inner and outer index ( or slot ) inside the <see cref="_entityInfos"/> array.
+    /// </summary>
+    /// <param name="id">The id.</param>
+    /// <param name="outerIndex">The outer index.</param>
+    /// <param name="innerIndex">The inner index.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void IdToSlot(int id, out int outerIndex, out int innerIndex)
+    {
+        Debug.Assert(id >= 0, "Id cannot be negative.");
+
+        outerIndex = id / _chunkSize;
+        /* Instead of the '%' operator we can use logical '&' operator which is faster. But it requires the chunk size to be a power of 2. */
+        innerIndex = id & _chunkSizeMinusOne;
+    }
+
+    /// <summary>
+    ///     Ensures the capacity of this <see cref="EntityInfoDictionary"/> and resizes it correctly.
+    /// </summary>
+    /// <param name="capacity">The new capacity.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void EnsureCapacity(int capacity)
+    {
+        if (capacity < _largestId)
+        {
+            return;
+        }
+
+        var currentSize = _entityInfos.Length;
+        var desiredSize = (capacity / _chunkSize) + 1;
+
+        Array.Resize(ref _entityInfos, desiredSize);
+
+        // Create the new arrays.
+        for (int i = currentSize; i < desiredSize; i++)
+        {
+            _entityInfos[i] = new EntityInfo[_chunkSize];
+        }
+
+        UpdateLargestId();
+    }
+
+    /// <summary>
+    ///     Trims this <see cref="EntityInfoDictionary"/> and releases unused resources.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void TrimExcess()
     {
         var lastIndexWithNonDefaultValues = _entityInfos.Length - 1;
-
         for (var i = lastIndexWithNonDefaultValues; i >= 0; i--)
         {
             if (ArrayContainsNonDefaultValues(_entityInfos[i]))
@@ -115,44 +178,33 @@ internal class EntityInfoDictionary
         }
 
         Array.Resize(ref _entityInfos, lastIndexWithNonDefaultValues + 1);
-
         UpdateLargestId();
     }
 
+    /// <summary>
+    ///     Clears this <see cref="EntityInfoDictionary"/> and sets all values to the default one.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Clear()
     {
         Array.Clear(_entityInfos, 0, _entityInfos.Length);
     }
 
-    public ref EntityInfo this[int id]
-    {
-        get
-        {
-            Debug.Assert(id >= 0, "Id cannot be negative");
-
-            EnsureCapacity(id);
-            GetIndexesFromId(id, out var outerIndex, out var innerIndex);
-            return ref _entityInfos[outerIndex][innerIndex];
-        }
-    }
-
-    private void GetIndexesFromId(int id, out int outerIndex, out int innerIndex)
-    {
-        Debug.Assert(id >= 0, "Id cannot be negative.");
-
-        outerIndex = id / _chunkSize;
-        //innerIndex = id % _chunkCount;
-        //innerIndex = id - (outerIndex * _chunkSize);
-
-        /* Instead of the '%' operator we can use logical '&' operator which is faster. But it requires the chunk size to be a power of 2. */
-        innerIndex = id & _chunkSizeMinusOne;
-    }
-
+    /// <summary>
+    ///     Updates the largest id.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private void UpdateLargestId()
     {
         _largestId = _entityInfos.Length * _chunkSize;
     }
 
+    /// <summary>
+    ///     Checks if the passed <see cref="EntityInfo"/> array contains set values.
+    /// </summary>
+    /// <param name="array">The <see cref="EntityInfo"/> array to check.</param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static bool ArrayContainsNonDefaultValues(EntityInfo[] array)
     {
         foreach (var item in array)
@@ -164,5 +216,22 @@ internal class EntityInfoDictionary
         }
 
         return false;
+    }
+
+    /// <summary>
+    ///     Returns a reference to a <see cref="EntityInfo"/> at an given index.
+    /// </summary>
+    /// <param name="id">The index.</param>
+    public ref EntityInfo this[int id]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            Debug.Assert(id >= 0, "Id cannot be negative");
+
+            EnsureCapacity(id);
+            IdToSlot(id, out var outerIndex, out var innerIndex);
+            return ref _entityInfos[outerIndex][innerIndex];
+        }
     }
 }
