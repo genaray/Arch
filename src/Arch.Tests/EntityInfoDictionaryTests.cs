8f38ba49ada6f2dd421b73c673cc570eb3fa626a
Szilágyi Krisztián <sz.krisz20010615@gmail.com>
2023-03-28 04:07:00 +0200

    Created a custom dictionary for the EntityInfo struct

---
+++ src/Arch.Tests/EntityInfoDictionaryTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arch.Core.Utils;
+
+namespace Arch.Tests;
+
+[TestFixture]
+public class EntityInfoDictionaryTests
+{
+    [Test]
+    public void Add()
+    {
+        var size = 1000;
+        var entityInfo = new EntityInfoDictionary(size);
+
+        
+        for (int i = 0; i < size; i++)
+        {
+            // Set the version to the current index.
+            var version = i;
+
+            entityInfo.Add(i, new Core.EntityInfo(default, default, version));
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            // Iterate over the dictionary if the index doesn't match the version then something went wrong
+            var version = entityInfo[i].Version;
+
+            if (version != i)
+            {
+                Assert.Fail("Dictionary does not returns the correct object");
+            }
+        }
+    }
+
+    [Test]
+    public void TrimExcess()
+    {
+        var size = 10000;
+        var entityInfo = new EntityInfoDictionary(size);
+
+        for(int i = 0; i < size; i++)
+        {
+            var version = i;
+
+            entityInfo.Add(i, new Core.EntityInfo(default, default, version));
+        }
+
+        entityInfo.TrimExcess();
+
+        for (int i = 0; i < size ; i++)
+        {
+            var version = entityInfo[i].Version;
+
+            if(version != i)
+            {
+                Assert.Fail($"{nameof(EntityInfoDictionary.TrimExcess)} method deleted items");
+            }
+        }
+    }
+}
---
+++ src/Arch/Core/Extensions/MathExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arch.Core.Extensions;
+internal static class MathExtensions
+{
+    /// <summary>
+    /// This method will round down to the nearest power of 2 number. If the supplied number is a power of 2 it will return it.
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public static int RoundToPowerOfTwo(int num)
+    {
+        // If num is a power of 2, return it
+        if (num > 0 && (num & (num - 1)) == 0)
+        {
+            return num;
+        }
+        
+        // Find the exponent of the nearest power of 2 (rounded down)
+        int exponent = (int)Math.Floor(Math.Log(num) / Math.Log(2));
+        
+        // Calculate the nearest power of 2
+        int result = (int)Math.Pow(2, exponent);
+        
+        return result;
+    }
+}
