b013c20b71a9cb528fc83de04be30fda20597db6
DrSmugleaf <drsmugleaf@gmail.com>
2023-05-15 00:00:41 -0700

    Split CreateRelationship test into NoRelationships and RelationshipCleanup, improve comments

--- src/Arch.Tests/RelationshipTest.cs
+++ src/Arch.Tests/RelationshipTest.cs
@@ -17,27 +17,25 @@ public class RelationshipTest
     }
 
     [Test]
-    public void CreateRelationship()
+    public void NoRelationships()
     {
-        _world.HandleRelationshipCleanup();
-
-        // Create without setting any relationships yet
+        // Create entities without setting any relationships
         var parent = _world.Create();
         var childOne = _world.Create();
         var childTwo = _world.Create();
 
-        // Get
+        // Get should throw
         Assert.That(() => _world.GetRelationships<ParentOf>(parent), Throws.Exception);
         Assert.That(() => _world.GetRelationships<ChildOf>(childOne), Throws.Exception);
         Assert.That(() => _world.GetRelationships<ChildOf>(childTwo), Throws.Exception);
 
-        // TryGet
+        // TryGet should return false
         Assert.False(_world.TryGetRelationships<ParentOf>(parent, out _));
         Assert.False(_world.TryGetRelationships<ChildOf>(childOne, out _));
         Assert.False(_world.TryGetRelationships<ChildOf>(childTwo, out _));
 
-        // TryGetRef
-        _world.TryGetRefRelationships<ParentOf>(parent, out var exists);
+        // TryGetRef should return a null ref and false
+        Assert.That(Unsafe.IsNullRef(ref _world.TryGetRefRelationships<ParentOf>(parent, out var exists)));
         Assert.False(exists);
 
         _world.TryGetRefRelationships<ChildOf>(childOne, out exists);
@@ -45,6 +43,18 @@ public class RelationshipTest
 
         _world.TryGetRefRelationships<ChildOf>(childTwo, out exists);
         Assert.False(exists);
+    }
+
+    [Test]
+    public void RelationshipCleanup()
+    {
+        // Setup handling relationship cleanup
+        _world.HandleRelationshipCleanup();
+
+        // Create entities
+        var parent = _world.Create();
+        var childOne = _world.Create();
+        var childTwo = _world.Create();
 
         // Add the relationships
         _world.AddRelationship<ParentOf>(parent, childOne);
@@ -52,14 +62,14 @@ public class RelationshipTest
         _world.AddRelationship<ChildOf>(childOne, parent);
         _world.AddRelationship<ChildOf>(childTwo, parent);
 
-        // Get
+        // Get should return a reference to the relationships
         Assert.That(_world.GetRelationships<ParentOf>(parent).Elements, Does
             .ContainKey(childOne).And
             .ContainKey(childTwo));
         Assert.That(_world.GetRelationships<ChildOf>(childOne).Elements, Does.ContainKey(parent));
         Assert.That(_world.GetRelationships<ChildOf>(childTwo).Elements, Does.ContainKey(parent));
 
-        // TryGet
+        // TryGet should return true and the relationships
         Assert.True(_world.TryGetRelationships<ParentOf>(parent, out var parentRelationships));
         Assert.That(parentRelationships.Elements, Does.ContainKey(childOne).And.ContainKey(childTwo));
 
@@ -69,8 +79,8 @@ public class RelationshipTest
         Assert.True(_world.TryGetRelationships<ChildOf>(childTwo, out childRelationships));
         Assert.That(childRelationships.Elements, Does.ContainKey(parent));
 
-        // TryGetRef
-        ref var parentRelationshipsRef = ref _world.TryGetRefRelationships<ParentOf>(parent, out exists);
+        // TryGetRef should return a reference to the relationships and true
+        ref var parentRelationshipsRef = ref _world.TryGetRefRelationships<ParentOf>(parent, out var exists);
         Assert.True(exists);
         Assert.That(parentRelationshipsRef.Elements, Does.ContainKey(childOne).And.ContainKey(childTwo));
 
@@ -85,7 +95,7 @@ public class RelationshipTest
         // Destroy childOne, should remove any relationships containing it
         _world.Destroy(childOne);
 
-        // Get
+        // Get should throw on childOne and not return it in any of the other references
         Assert.That(_world.GetRelationships<ParentOf>(parent).Elements, Does.Not.ContainKey(childOne).And.ContainKey(childTwo));
         Assert.That(() => _world.GetRelationships<ChildOf>(childOne).Elements, Throws.Exception);
         Assert.That(_world.GetRelationships<ChildOf>(childTwo).Elements, Does.ContainKey(parent));
@@ -93,26 +103,30 @@ public class RelationshipTest
         // Destroy childTwo, should remove any relationships containing it
         _world.Destroy(childTwo);
 
-        // Get
+        // Get, all should throw as no relationships are left
         Assert.That(() => _world.GetRelationships<ParentOf>(parent), Throws.Exception);
         Assert.That(() => _world.GetRelationships<ChildOf>(childOne), Throws.Exception);
         Assert.That(() => _world.GetRelationships<ChildOf>(childTwo), Throws.Exception);
 
-        _world.Destroy(parent);
+        // Destroy parent
+        Assert.DoesNotThrow(() => _world.Destroy(parent));
     }
 
     [Test]
     public void QueryRelationship()
     {
+        // Create entities
         var parent = _world.Create();
         var childOne = _world.Create();
         var childTwo = _world.Create();
 
+        // Add relationships
         _world.AddRelationship<ParentOf>(parent, childOne);
         _world.AddRelationship<ParentOf>(parent, childTwo);
         _world.AddRelationship<ChildOf>(childOne, parent);
         _world.AddRelationship<ChildOf>(childTwo, parent);
 
+        // Query all ParentOf relationships
         var query = new QueryDescription().WithAll<EntityRelationshipBuffer<ParentOf>>();
         var entities = new List<Entity>();
         _world.Query(query, (in Entity _, ref EntityRelationshipBuffer<ParentOf> parentOf) =>
@@ -120,6 +134,7 @@ public class RelationshipTest
             entities.AddRange(parentOf.Elements.Select(p => p.Key));
         });
 
+        // Should find two ParentOf relationships, from childOne and childTwo
         Assert.That(entities, Has.Count.EqualTo(2));
         Assert.That(entities, Does.Contain(childOne));
         Assert.That(entities, Does.Contain(childTwo));
