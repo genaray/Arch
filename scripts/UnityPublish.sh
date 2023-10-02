fa8f973ac5047be823dcc263e0e0d6ff881dbc3e
Reese Schultz <contact@reese.codes>
2023-10-01 21:31:42 -0500

    Ensure Unity publishing works without disrupting other targets

--- scripts/UnityPublish.sh
+++ scripts/UnityPublish.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Publishes Unity release to dist/Assemblies using only netstandard2.0 and netstandard2.1
-# Does NOT include source generation
 #########################################################################################
 
 dotnet restore
 
 assemblyDir="`pwd`/dist/Assemblies"
 
+rm -rf "${assemblyDir}"
+
 mkdir -p "${assemblyDir}"
 
-dotnet msbuild /t:Unity -p:PublishDir="${assemblyDir}"
+dotnet msbuild /t:Unity \
+    -p:PublishDir="${assemblyDir}" \
+    -p:TargetFramework=netstandard2.1 \
+    -p:TargetFrameworks=netstandard2.1 \
+    -p:TargetFrameworkVersion=v2.1
