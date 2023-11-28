0afd40a1f0b5a1375a15c8c13500d3f653093490
Roycon <4994730+RoyconSkylands@users.noreply.github.com>
2023-11-28 16:22:29 +0000

    Fixing sizes (#189)
    * Increased array size for when adding new components
    
    * Maybe better

--- src/Arch.SourceGen/Fundamentals/StructuralChanges.cs
+++ src/Arch.SourceGen/Fundamentals/StructuralChanges.cs
@@ -19,11 +19,13 @@ public static class StructuralChangesExtensions
         var inParameters = new StringBuilder().InsertGenericInParams(amount);
         var types = new StringBuilder().GenericTypeParams(amount);
 
+        var getIds = new StringBuilder();
         var setIds = new StringBuilder();
         var addEvents = new StringBuilder();
         for (var index = 0; index <= amount; index++)
         {
-            setIds.AppendLine($"spanBitSet.SetBit(Component<T{index}>.ComponentType.Id);");
+            getIds.AppendLine($"var id{index} = Component<T{index}>.ComponentType.Id;");
+            setIds.AppendLine($"spanBitSet.SetBit(id{index});");
             addEvents.AppendLine($"OnComponentAdded<T{index}>(entity);");
         }
 
@@ -35,6 +37,9 @@ public static class StructuralChangesExtensions
             public void Add<{{generics}}>(Entity entity, {{parameters}})
             {
                 var oldArchetype = EntityInfo.GetArchetype(entity.Id);
+
+                // Get all the ids here just in case we are adding a new component as this will grow the ComponentRegistry.Size
+                {{getIds}}
 
                 // BitSet to stack/span bitset, size big enough to contain ALL registered components.
                 Span<uint> stack = stackalloc uint[BitSet.RequiredLength(ComponentRegistry.Size)];
--- src/Arch.SourceGen/Queries/AddWithQueryDescription.cs
+++ src/Arch.SourceGen/Queries/AddWithQueryDescription.cs
@@ -50,7 +50,7 @@ public static class AddWithQueryDescription
             public void Add<{{generics}}>(in QueryDescription queryDescription, {{parameters}})
             {
                 // BitSet to stack/span bitset, size big enough to contain ALL registered components.
-                Span<uint> stack = stackalloc uint[BitSet.RequiredLength(ComponentRegistry.Size)];
+                Span<uint> stack = stackalloc uint[BitSet.RequiredLength(ComponentRegistry.Size + {{amount + 1}})];
 
                 var query = Query(in queryDescription);
                 foreach (var archetype in query.GetArchetypeIterator())
