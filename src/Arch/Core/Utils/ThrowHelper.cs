b44f8f9f7fdfd0b903d6896117677bd0d8435bdb
AnnulusGames <annulusgames@gmail.com>
2024-05-08 09:11:05 +0900

    Changed archetype movement assertion to exception

--- src/Arch/Core/Utils/ThrowHelper.cs
+++ src/Arch/Core/Utils/ThrowHelper.cs
@@ -7,12 +7,12 @@ internal static class ThrowHelper
     [DoesNotReturn]
     public static void Throw_ComponentDoesNotExists(ComponentType type)
     {
-        throw new ArgumentException($"Component {type.Type} does not exists");
+        throw new InvalidOperationException($"You are trying to access a component({type}) that does not exist in the entity or chunk.");
     }
 
     [DoesNotReturn]
-    public static void Throw_EntityDoesNotExists()
+    public static void Throw_SameArchetype()
     {
-        throw new ArgumentException($"Entity does not exists");
+        throw new InvalidOperationException($"From-Archetype is the same as the To-Archetype. Entities cannot move within the same archetype using this function. Probably an attempt was made to attach already existing components to the entity or to remove non-existing ones.");
     }
 }
